caa76f471582aa223eb1be920fe6f4ac20f5c038
talha-webozza <talha@webozza.com>
2023-10-19 19:01:49 +0600

    js

--- js/main.js
+++ js/main.js
@@ -117,7 +117,7 @@ jQuery(document).ready(function ($) {
       type: "GET",
       dataType: "json",
       success: function (data) {
-        $('.popup-box').fadeIn(500).css({ 'transform': 'scale(1.2)' });
+        $('.popup-box').fadeIn(300).css({ 'transform': 'scale(1)' });
 
         // const title = data.title.rendered;
         // const content = data.content.rendered; 
