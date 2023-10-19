9df73aac2892c24db47b12fd8685899cf15ec8a4
talha-webozza <talha@webozza.com>
2023-10-19 18:40:55 +0600

    css

--- module/home-grid.php
+++ module/home-grid.php
@@ -8,6 +8,21 @@ $query = new WP_Query($args);
 
 ?>
 <div class="my-masonry-grid">
+    
+<div class="popup-box">
+      <div class="img">
+        <img src="el.jpg" alt="" />
+      </div>
+      <div class="navigation">
+        <div class="plus"><img src="popUpIcon/icono-plus.png" alt="" /></div>
+        <div class="cross"><img src="popUpIcon/icono-x.png" alt="" /></div>
+        <div class="zoom"><img src="popUpIcon/icono-lupa+.png" alt="" /></div>
+        <div class="document"><img src="popUpIcon/icono-documentos.png" alt="" /></div>
+      </div>
+      <div class="documentWindow">
+
+      </div>
+    </div>
     <?php if ($query->have_posts()) : ?>
     <?php while ($query->have_posts()) : $query->the_post(); ?>
     <?php
@@ -28,18 +43,3 @@ $query = new WP_Query($args);
     <?php endif; ?>
 </div>
 
-
-<div class="popup-box">
-      <div class="img">
-        <img src="el.jpg" alt="" />
-      </div>
-      <div class="navigation">
-        <div class="plus"><img src="popUpIcon/icono-plus.png" alt="" /></div>
-        <div class="cross"><img src="popUpIcon/icono-x.png" alt="" /></div>
-        <div class="zoom"><img src="popUpIcon/icono-lupa+.png" alt="" /></div>
-        <div class="document"><img src="popUpIcon/icono-documentos.png" alt="" /></div>
-      </div>
-      <div class="documentWindow">
-
-      </div>
-    </div>
