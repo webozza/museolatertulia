841e0a53ba1928a954e3249a3983fb2cc7e07730
talha-webozza <talha@webozza.com>
2023-10-20 12:45:28 +0600

    js

--- js/main.js
+++ js/main.js
@@ -162,21 +162,24 @@ jQuery(document).ready(function ($) {
       maxScale: 3,
       minScale: 0.5,
     });
+  
     $(".zoom").on("click", () => {
       panzoom.zoom(2);
-
-      $(".zoom").hide()
-      $('.zoomOut').show()
+      $(".zoom").hide();
+      $('.zoomOut').show();
+      zoomImage.style.transition = "transform 0.3s"; // Add a smooth transition
     });
+  
     $(".zoomOut").on("click", () => {
-      panzoom.zoomOut(originalScale);
-
-      $(".zoom").show()
-      $('.zoomOut').hide()
+      panzoom.zoom(1); // Zoom back to the original scale (1)
+      $(".zoom").show();
+      $('.zoomOut').hide();
+      zoomImage.style.transition = "transform 0.3s"; // Add a smooth transition
     });
-
   };
-
+  
+  makeZoom();
+  
 
   //============================
   //                        Nav Button Click
