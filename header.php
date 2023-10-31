f18600ed1d6c51ce2ab0157a693679a6885f50fa
talha-webozza <talha@webozza.com>
2023-10-31 15:26:58 +0600

    menu id

--- header.php
+++ header.php
@@ -161,4 +161,22 @@ function querry_menu($menu_category){
             })
         })
 
+        $('.drop_down_menu').click(function () {
+        let parentMenu = $(this).parent().parent().find('.parent_menu').attr('id');
+        let id = $(this).attr('id');
+
+        $.ajax({
+            url: '/wp-admin/admin-ajax.php',
+            type: 'post',
+            data: {
+                action: 'get_filtered_img',
+                parentMenu: parentMenu,
+                menuId: id
+            },
+            success: function(response) {
+                console.log(response);
+            }
+        });
+    });
+
     </script> 
--- module/home-grid.php
+++ module/home-grid.php
@@ -111,23 +111,7 @@ $query = new WP_Query($args);
   //                             Filter left menu 
   // =================================//
 
-      $('.drop_down_menu').click(function () {
-        let parentMenu = $(this).parent().parent().find('.parent_menu').attr('id');
-        let id = $(this).attr('id');
 
-        $.ajax({
-            url: '/wp-admin/admin-ajax.php',
-            type: 'post',
-            data: {
-                action: 'get_filtered_img',
-                parentMenu: parentMenu,
-                menuId: id
-            },
-            success: function(response) {
-                console.log(response);
-            }
-        });
-    });
 
 
 
