8198e5df9d7f3a2a45dcaac1c9c519f41be61be0
talha-webozza <talha@webozza.com>
2023-11-01 15:38:23 +0600

    version update

--- module/map.php
+++ module/map.php
@@ -1,6 +1,6 @@
 <div class="map-body">
     <div class="map">
-        <? include get_stylesheet_directory_uri(  ).'/src/map_svj.php'?>
+        <?php include get_stylesheet_directory_uri(  ).'/src/map_svj.php'?>
     </div>
     <div class="map_filterd_data">
 
