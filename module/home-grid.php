07717127ce04de9f807be5b8a6409afccc59ec6b
talha-webozza <talha@webozza.com>
2023-11-01 12:58:33 +0600

    version update

--- module/home-grid.php
+++ module/home-grid.php
@@ -126,7 +126,7 @@ $query = new WP_Query($args);
           $('.my-masonry-grid').html(response)
           $('.my-masonry-grid').prepend(preLoader)
           setTimeout(() => {
-            ('.pre-loader-filtered').fadeOut()
+            $('.pre-loader-filtered').fadeOut()
           }, 2000);
           $(".my-masonry-grid").masonryGrid({
             columns: 6,
