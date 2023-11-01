861128fc90edaf7b5f5d58d6d2d86a8e76cdb160
talha-webozza <talha@webozza.com>
2023-11-01 12:23:20 +0600

    version update

--- module/home-grid.php
+++ module/home-grid.php
@@ -16,28 +16,28 @@ $query = new WP_Query($args);
             <img class="main_image" src="" alt="" id="zoom-image">
         </div>
       <div class="navigation">
-        <div class="plus"><img src="<?= get_stylesheet_directory_uri();?>/popUpIcon/icono-plus.png" alt="" /></div>
-        <div class="cross"><img src="<?= get_stylesheet_directory_uri();?>/popUpIcon/icono-x.png" alt="" /></div>
-        <div class="zoom"><img src="<?= get_stylesheet_directory_uri();?>/popUpIcon/icono-lupa+.png" alt="" /></div>
-        <div class="zoomOut"><img src="<?= get_stylesheet_directory_uri();?>/popUpIcon/icono-minus.png" alt="" /></div>
-        <div class="document"><img src="<?= get_stylesheet_directory_uri();?>/popUpIcon/icono-documentos.png" alt="" /></div>
+            <div class="plus"><img src="<?= get_stylesheet_directory_uri();?>/popUpIcon/icono-plus.png" alt="" /></div>
+            <div class="cross"><img src="<?= get_stylesheet_directory_uri();?>/popUpIcon/icono-x.png" alt="" /></div>
+            <div class="zoom"><img src="<?= get_stylesheet_directory_uri();?>/popUpIcon/icono-lupa+.png" alt="" /></div>
+            <div class="zoomOut"><img src="<?= get_stylesheet_directory_uri();?>/popUpIcon/icono-minus.png" alt="" /></div>
+            <div class="document"><img src="<?= get_stylesheet_directory_uri();?>/popUpIcon/icono-documentos.png" alt="" /></div>
       </div>
       <div class="info">
-        <p ><strong> <span class="title"></span></strong></p>
-        <p ><strong> <span class="author"></span></strong></p>
-        <br>
-        <p> <span class="dimension"></span></p>
-        <p> <span class="edition"></span></p>
-        <p><strong>Técnica : </strong> <span class="technique"></span></p>
-        <p><strong>Nacionalidad : </strong> <span class="nationality"></span></p>
-        <br>
-        <p ><strong>Categoría : </strong> <span class="catagories"></span></p>
-        <p ><strong>TAGS : </strong> <span class="tags"></span></p>
-        <br>
-        <p ><strong>Tipo Documental : </strong> <span class="documents"></span></p>
-        <p><strong>Fuente y notas: : </strong> <span class="source"></span></p>
-        <br>
-        <p><strong>Otras colecciones : </strong> <span class="other-ducuments"></span></p>
+            <p ><strong> <span class="title"></span></strong></p>
+            <p ><strong> <span class="author"></span></strong></p>
+            <br>
+            <p> <span class="dimension"></span></p>
+            <p> <span class="edition"></span></p>
+            <p><strong>Técnica : </strong> <span class="technique"></span></p>
+            <p><strong>Nacionalidad : </strong> <span class="nationality"></span></p>
+            <br>
+            <p ><strong>Categoría : </strong> <span class="catagories"></span></p>
+            <p ><strong>TAGS : </strong> <span class="tags"></span></p>
+            <br>
+            <p ><strong>Tipo Documental : </strong> <span class="documents"></span></p>
+            <p><strong>Fuente y notas: : </strong> <span class="source"></span></p>
+            <br>
+            <p><strong>Otras colecciones : </strong> <span class="other-ducuments"></span></p>
       </div>
       <div class="documentWindow">
         <div class="documentData ">
@@ -121,6 +121,10 @@ $query = new WP_Query($args);
 				success: function(response) {
           console.log(response)
           $('.my-masonry-grid').html(response)
+          $('.my-masonry-grid').prepend($('.pre-loader'))
+          setTimeout(() => {
+          $('.pre-loader').fadeOut()
+          }, 2000);
           $(".my-masonry-grid").masonryGrid({
             columns: 6,
           });
