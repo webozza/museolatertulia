a336ea826ee1d8935c8c4ec5e2a47778b5d6b58b
talha-webozza <talha@webozza.com>
2023-11-02 14:45:41 +0600

    version update

--- module/categories.php
+++ module/categories.php
@@ -1,12 +1,18 @@
 <?php
         $terms = get_terms('categoria');
+
         if (!empty($terms)) {
+            echo '<ul>';
             foreach ($terms as $term) {
-                echo 'Term Name: ' . $term->name . '<br>';
-                echo 'Term ID: ' . $term->term_id . '<br>';
-                echo 'Term Slug: ' . $term->slug . '<br>';
-                echo '<br>';
+                // Get the number of posts associated with the term
+                $term_count = $term->count;
+
+                echo '<li class="' . $term->slug . '">';
+                echo 'Term Name: ' . $term->name;
+                echo ' (Posts: ' . $term_count . ')';
+                echo '</li>';
             }
+            echo '</ul>';
         } else {
             echo 'No terms found in the "categoria" taxonomy.';
         }
