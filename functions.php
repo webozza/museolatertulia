1adeafda96a7a0cf7afe68ad3f6e165b89ee1951
talha-webozza <talha@webozza.com>
2023-11-02 17:27:15 +0600

    version update

--- functions.php
+++ functions.php
@@ -258,32 +258,36 @@ function catagoryFilter(){
     $args = array(
         'post_type' => 'obra',
         'posts_per_page' => -1,
-        'meta_query' => array(
+        'tax_query' => array(  // Use 'tax_query' instead of 'meta_query'
             array(
                 'taxonomy' => 'categoria',
+                'field' => 'slug',  // Specify the field, e.g., 'slug'
                 'terms' => $terms,
             ),
         ),
     );
     $query = new WP_Query($args);
-    ?>
-    <?php if ($query->have_posts()) : ?>
-            <?php while ($query->have_posts()) : $query->the_post(); ?>
-            <?php $post_id = get_the_ID();?>
-                    <div class="my-masonry-grid-item">
-                        <?php the_post_thumbnail('large',
-                            array(
-                                'class' => 'clickable-thumbnail',
-                                'data-post-id' => $post_id, 
-                            ));
-                        ?>
-                    </div>
-            <?php endwhile; ?>
-            <?php wp_reset_postdata(); ?>
-            <?php else : ?>
-            <p>No posts found.</p>
-            <?php endif; ?>
-    <?php
+
+    if ($query->have_posts()) {
+        while ($query->have_posts()) {
+            $query->the_post();
+            $post_id = get_the_ID();
+            ?>
+            <div class="my-masonry-grid-item">
+                <?php the_post_thumbnail('large',
+                    array(
+                        'class' => 'clickable-thumbnail',
+                        'data-post-id' => $post_id,
+                    ));
+                ?>
+            </div>
+            <?php
+        }
+        wp_reset_postdata();
+    } else {
+        echo 'No posts found.';
+    }
+    wp_die();
 }
 
 add_action('wp_ajax_catagoryFilter', 'catagoryFilter');
