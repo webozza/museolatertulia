bb5c2dd2f4be5fe13982b7b2c865c010ed4a917c
talha-webozza <talha@webozza.com>
2023-11-07 12:59:02 +0600

    version update

--- module/page_poup/el_projecto.php
+++ module/page_poup/el_projecto.php
@@ -4,8 +4,16 @@ $slug = 'el-projecto'; // Replace with the slug of the page you want to retrieve
 $page = get_page_by_path($slug);
 
 if ($page) {
-    echo '<h1>' . $page->post_title . '</h1>'; // Display the page title
-    echo apply_filters('the_content', $page->post_content); // Display the page content
+    ?>
+    <div class="thumbnail">
+        <?php
+        if (has_post_thumbnail($page->ID)) {
+            echo get_the_post_thumbnail($page->ID, 'large');
+        }
+        ?>
+    </div>
+    <div class="content"><?= $apply_filters('the_content', $page->post_content); ?></div>
+    <?php
 } else {
     echo 'Page not found';
 }
