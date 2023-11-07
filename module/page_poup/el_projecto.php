8f584ce9c2f7889e597f22415b4e001f42d33a9a
talha-webozza <talha@webozza.com>
2023-11-07 12:35:29 +0600

    version update

--- module/page_poup/el_projecto.php
+++ module/page_poup/el_projecto.php
@@ -4,16 +4,16 @@ $page_id = 4450;
 $page = get_post($page_id);
 
 if ($page) {
-    ?>
-    <div class="thumbnail">
-        <?php
-        if (has_post_thumbnail($page->ID)) {
-            echo get_the_post_thumbnail($page->ID, 'large');
-        }
-        ?>
-    </div>
-    <div class="content"><?= $page->post_content ?></div>
-    <?php
+    // Display the page title
+    echo '<h1>' . $page->post_title . '</h1>';
+
+    // Display the post thumbnail
+    if (has_post_thumbnail($page->ID)) {
+        echo get_the_post_thumbnail($page->ID, 'large'); // 'large' is the image size, change it if needed
+    }
+
+    // Display the page content
+    echo $page->post_content;
 } else {
     echo 'Page not found';
 }
