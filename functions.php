eae361760cb5bbda65f2868a20e34ed07b4cd56d
talha-webozza <talha@webozza.com>
2023-11-08 14:25:44 +0600

    year filter update

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 if (!defined('_T_VERSION')) {
-    define('_T_VERSION', '1.0.55');
+    define('_T_VERSION', '1.0.56');
 }
 
 
--- module/map.php
+++ module/map.php
@@ -66,6 +66,9 @@
             position: relative;
             height: 100%;
             width: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
         }
         .map_container #zoom-image {
             display: flex;
